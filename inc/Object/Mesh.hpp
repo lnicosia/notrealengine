1db6b36b19978d108b803a720aaec4bbff8c3b88
lnicosia <lnicosia@student.42.fr>
2021-11-18 15:10:25 +0100

    [ADD] Mesh.draw() and Mesh.setup().

--- inc/Object/Mesh.hpp
+++ inc/Object/Mesh.hpp
@@ -1,8 +1,21 @@
 #ifndef _MESH_HPP_
 # define _MESH_HPP_
 
-#include "Object/Vertex.hpp"
+#include "mft/mft.hpp"
 #include <vector>
+
+struct Vertex
+{
+	mft::vec3	pos;
+	mft::vec3	norm;
+	mft::vec2	uv;
+};
+
+struct Texture
+{
+	unsigned int	id;
+	std::string		type;
+};
 
 class Mesh
 {
@@ -13,20 +26,33 @@ class Mesh
 		// to initiate meshes from assimp
 		Mesh(std::vector<Vertex> vertices,
 			std::vector<unsigned int> indices,
-			std::vector<unsigned int> textures);
+			std::vector<Texture> textures);
 		~Mesh();
 		Mesh & operator=(Mesh const& mesh);
 
 		void	draw() const;
 
+		// Accessors
+
+		unsigned int const	getVAO();
+		unsigned int const	getVBO();
+		unsigned int const	getEBO();
+
+		unsigned int const	getPolygonMode();
+
 	private:
 		std::vector<Vertex>			vertices;
 		std::vector<unsigned int>	indices;
-		std::vector<unsigned int>	textures;
+		std::vector<Texture>		textures;
 
 		std::string	name;
 
 		unsigned int	VAO, VBO, EBO;
+
+		unsigned int	polygon_mode;
+
+		//		Initalize buffers and vertex array for the mesh
+		//		Should not be called outside of the class scope
 		void	setup();
 };
 
--- src/Object/Mesh.cpp
+++ src/Object/Mesh.cpp
@@ -6,8 +6,11 @@
 	# define GL_GLEXT_PROTOTYPES
 	#include <GL/gl.h>
 #endif
+#include <string>
 
-Mesh::Mesh(): VAO(0), VBO(0), EBO(0)
+//	Constructors
+
+Mesh::Mesh(): VAO(0), VBO(0), EBO(0), polygon_mode(GL_FILL)
 {
 }
 
@@ -34,9 +37,9 @@ Mesh::~Mesh()
 
 Mesh::Mesh(std::vector<Vertex> vertices,
 	std::vector<unsigned int> indices,
-	std::vector<unsigned int> textures)
+	std::vector<Texture> textures)
 	: vertices(vertices), indices(indices), textures(textures),
-		VAO(0), VBO(0), EBO(0)
+		VAO(0), VBO(0), EBO(0), polygon_mode(GL_FILL)
 {
 	setup();
 }
@@ -52,14 +55,54 @@ Mesh& Mesh::operator=(Mesh const& mesh)
 	this->VBO = mesh.VBO;
 	this->VAO = mesh.VAO;
 	this->EBO = mesh.EBO;
+
+	this->polygon_mode = mesh.polygon_mode;
 	return *this;
+}
+
+//	Accessors
+
+unsigned int const	Mesh::getVAO()
+{
+	return VAO;
+}
+
+unsigned int const	Mesh::getVBO()
+{
+	return VBO;
+}
+
+unsigned int const	Mesh::getEBO()
+{
+	return EBO;
+}
+
+unsigned int const	Mesh::getPolygonMode()
+{
+	return polygon_mode;
 }
 
 void	Mesh::draw() const
 {
+	unsigned int	diffuse = 0;
+	unsigned int	specular = 0;
+	for (size_t i = 0; i < textures.size(); i++)
+	{
+		glActiveTexture(GL_TEXTURE0 + (unsigned int)i);
 
+		std::string	nb;
+		if (textures[i].type == "texture_diffuse")
+			nb = std::to_string(diffuse);
+		else if (textures[i].type == "texture_specular")
+			nb = std::to_string(specular);
+		glBindTexture(GL_TEXTURE_2D, textures[i].id);
+	}
+	glPolygonMode(GL_FRONT_AND_BACK, polygon_mode);
+	glDrawElements(GL_TRIANGLES, (int)indices.size(), GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
 }
 
+//		Initalize buffers and vertex array for the mesh
 void	Mesh::setup()
 {
 	glGenVertexArrays(1, &VAO);
@@ -69,10 +112,10 @@ void	Mesh::setup()
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
-	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, (long)vertices.size() * ((long)sizeof(Vertex)), &vertices[0], GL_STATIC_DRAW);
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int),
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, (long)indices.size() * ((long)sizeof(unsigned int)),
 		&indices[0], GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);
