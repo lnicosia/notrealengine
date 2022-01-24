b0ec4ed9dc9991fdc9ba89987177264fa17262dd
lnicosia <lnicosia@student.42.fr>
2022-01-24 17:53:19 +0100

    [FIX] Visual compilator uses C standard for variadic macros.

--- inc/notrealengine/GL.hpp
+++ inc/notrealengine/GL.hpp
@@ -13,11 +13,8 @@
 # include "GLException.class.hpp"
 # include <filesystem>
 
-# if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-# define GLCallThrow(func_name, ...) GLCallTemplate<decltype(func_name)>::Call(#func_name, func_name __VA_OPT__, __VA_ARGS__)
-# else
-# define GLCallThrow(func_name, ...) GLCallTemplate<decltype(func_name)>::Call(#func_name, func_name __VA_OPT__(,) __VA_ARGS__)
-#endif
+# define GLCallThrow(func_name, ...) GLCallTemplate<decltype(func_name)>::Call(#func_name, func_name, __FILE__, __LINE__, #__VA_ARGS__ __VA_OPT__(,) __VA_ARGS__)
+
 
 namespace notrealengine
 {
@@ -25,18 +22,18 @@ namespace notrealengine
 	struct GLCallTemplate;
 
 	template<typename R, typename ... Args>
-	struct GLCallTemplate<R (*)(Args...)>
+	struct GLCallTemplate<R(*)(Args...)>
 	{
-		static R Call(std::string func_name, R (* GLFunction)(Args ...), Args ... args)
+		static R Call(std::string func_name, R(*GLFunction)(Args ...), std::string file, int line, std::string arg, Args ... args)
 		{
 			GLenum error;
 			error = glGetError();
 			if (error != GL_NO_ERROR)
-				throw GLException( "Previous OpenGL function failed and error wasn't checked", error );
+				throw GLException("Previous OpenGL function failed and error wasn't checked", error);
 			R ret = GLFunction(std::forward<Args>(args)...);
 			error = glGetError();
 			if (error != GL_NO_ERROR)
-				throw GLException( "OpenGL function '" + func_name + "' failed.", error );
+				throw GLException("\n" + file + ":" + std::to_string(line - 1) + ":\nOpenGL function '" + func_name + "(" + arg + ")' failed.", error);
 			return ret;
 		};
 	};
@@ -44,52 +41,51 @@ namespace notrealengine
 	template<typename ... Args>
 	struct GLCallTemplate<void (*)(Args...)>
 	{
-		static void Call(std::string func_name, void (* GLFunction)(Args ...), Args ... args)
+		static void Call(std::string func_name, void (*GLFunction)(Args ...), std::string file, int line, std::string arg, Args ... args)
 		{
 			GLenum error;
 			error = glGetError();
 			if (error != GL_NO_ERROR)
-				throw GLException( "Previous OpenGL function failed and error wasn't checked", error );
+				throw GLException("Previous OpenGL function failed and error wasn't checked", error);
 			GLFunction(std::forward<Args>(args)...);
 			error = glGetError();
 			if (error != GL_NO_ERROR)
-				throw GLException( "OpenGL function '" + func_name + "' failed.", error );
+				throw GLException("\n" + file + ":" + std::to_string(line - 1) + ":\nOpenGL function '" + func_name + "(" + arg + ")' failed.", error);
 		};
 	};
 
 	template<typename R, typename ... Args>
 	struct GLCallTemplate<R(Args...)>
 	{
-		static R Call(std::string func_name, R GLFunction(Args ...), Args ... args)
+		static R Call(std::string func_name, R GLFunction(Args ...), std::string file, int line, std::string arg, Args ... args)
 		{
 			GLenum error;
 			error = glGetError();
 			if (error != GL_NO_ERROR)
-				throw GLException( "Previous OpenGL function failed and error wasn't checked", error );
+				throw GLException("Previous OpenGL function failed and error wasn't checked", error);
 			R ret = GLFunction(std::forward<Args>(args)...);
 			error = glGetError();
 			if (error != GL_NO_ERROR)
-				throw GLException( "OpenGL function '" + func_name + "' failed.", error );
+				throw GLException("\n" + file + ":" + std::to_string(line - 1) + ":\nOpenGL function '" + func_name + "(" + arg + ")' failed.", error);
 			return ret;
 		};
 	};
 
 	template<typename ... Args>
-	struct GLCallTemplate<void (Args...)>
+	struct GLCallTemplate<void(Args...)>
 	{
-		static void Call(std::string func_name, void GLFunction(Args ...), Args ... args)
+		static void Call(std::string func_name, void GLFunction(Args ...), std::string file, int line, std::string arg, Args ... args)
 		{
 			GLenum error;
 			error = glGetError();
 			if (error != GL_NO_ERROR)
-				throw GLException( "Previous OpenGL function failed and error wasn't checked", error );
+				throw GLException("Previous OpenGL function failed and error wasn't checked", error);
 			GLFunction(std::forward<Args>(args)...);
 			error = glGetError();
 			if (error != GL_NO_ERROR)
-				throw GLException( "OpenGL function '" + func_name + "' failed.", error );
+				throw GLException("\n" + file + ":" + std::to_string(line - 1) + ":\nOpenGL function '" + func_name + "(" + arg + ")' failed.", error);
 		};
 	};
 }
 
 #endif
-
