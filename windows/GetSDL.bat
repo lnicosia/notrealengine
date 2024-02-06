07521fcef358f116846ca8d56c61a0e3e10232c1
Lucas Nicosia <44832754+lnicosia@users.noreply.github.com>
2024-02-06 13:52:20 +0100

    [ADD] GetSDL.bat creates windows junctions (symlinks) to src/ and inc/ in the windows/ folder

--- windows/GetSDL.bat
+++ windows/GetSDL.bat
@@ -4,3 +4,11 @@ SetLocal EnableDelayedExpansion
 if not exist ..\lib\SDL\include\SDL.h (
 	git submodule update --init ..\lib\SDL
 )
+
+if not exist src (
+	mklink /j src ..\src
+)
+
+if not exist inc (
+	mklink /j inc ..\inc
+)
